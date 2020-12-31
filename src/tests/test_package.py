8998b334195d4fa9f48e164c223ecd2ccc3e36d3
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2020-12-31 10:08:52 -0500

    working on coverage report

--- src/tests/test_package.py
+++ src/tests/test_package.py
@@ -11,24 +11,53 @@ except ImportError:
     is_pypy = False
 
 
-class Test(object):
+class Test:
 
     # See what Python versions the combined
     # coverage report includes
     def test_show_coverage(self):
-        assert sys.version_info.major == 3
-        if not is_pypy:
-            if sys.version_info.minor == 6:
-                print(sys.version_info)
-            elif sys.version_info.minor == 7:
-                print(sys.version_info)
-            elif sys.version_info.minor == 8:
-                print(sys.version_info)
-            elif sys.version_info.minor == 9:
-                print(sys.version_info)
-        if is_pypy:
-            if sys.version_info.minor == 6:
-                print(sys.version_info)
+        if sys.platform.startswith("linux"):
+            assert sys.version_info.major == 3
+            if not is_pypy:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 7:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 8:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 9:
+                    print(sys.version_info)
+            if is_pypy:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
+        elif sys.platform.startswith("darwin"):
+            assert sys.version_info.major == 3
+            if not is_pypy:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 7:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 8:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 9:
+                    print(sys.version_info)
+            if is_pypy:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
+        elif sys.platform.startswith("win32"):
+            assert sys.version_info.major == 3
+            if not is_pypy:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 7:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 8:
+                    print(sys.version_info)
+                elif sys.version_info.minor == 9:
+                    print(sys.version_info)
+            if is_pypy:
+                if sys.version_info.minor == 6:
+                    print(sys.version_info)
 
     def test_version(self):
         pyomo_kernel_extensions.__version__
